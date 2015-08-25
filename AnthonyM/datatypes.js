60337afb80a2dcbb9bc9b3501701b666471e5443
mugiluri <anthonyhost6@gmail.com>
2015-08-25 18:16:02 +0300

    Add the file with the datatype identifier function ::smiley::

--- AnthonyM/datatypes.js
+++ AnthonyM/datatypes.js
@@ -1,22 +1,25 @@
-var datatypes = function(x){
+var datatypes = function(x) {
     var type = typeof x;
-  
     if (type === 'number') {
-  if (x % 1 === 0) {
+    if (x % 1 === 0) {
       return "Integer";
   }
- 
   {
-     return "float"; 
-  }
-       
+    return "float"; 
+  }   
     }
-  else if  (type === 'string'){
-      return "string";
+  else if (type === 'string') {
+    return "string";
   }
-  else if (type === 'Object') {
-      return "Objects";
+  else if (type === 'object') {
+    return "Objects";
+  }
+  else if (type === 'null') {
+    return "null";
+  }
+  else if (type === 'boolean') {
+    return "Boolean";
   }
 };
-console.log(datatypes(2.34));
-/*Test the code with any values*/
+console.log(datatypes(3.142));
+/*Test the code with any values__Mugiluri__Anthony*/
