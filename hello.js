188f11eb80331a7677fd9951513ec7c6383e519c
roksta <mbuthias21@gmail.com>
2015-08-25 12:46:58 +0300

    test

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-
+ function rev1(A){
+     for (i = 0; i < (Math.floor(A.length / 2)); i++){
+         temp = A[i];
+         A[i] = A[A.length-1-i] ;
+         A[A.length-1-i] = temp;
+     }
+ }
+ rev1(A);
+ console.log(A);
