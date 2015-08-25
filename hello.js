87c0216d172ea4a0e34e79bfdc8c3c2426e4fbc8
roksta <mbuthias21@gmail.com>
2015-08-25 12:44:26 +0300

    test2

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
-
+  function solution(B){
+     for (i = 1; i <= (B.length + 1); i++){
+         var x = 0;
+         for (j = 0; j < (B.length); j++){
+            if (i == B[j]){
+               // console.log (i +" is there")
+                break;
+            } else {
+                x = x + 1;
+            }
+            if (x == B.length){
+                console.log(i + " is missing");
+            }
+         }
+     }
+ }
+ var B=[1, 3, 5, 2];
+ solution(B);
