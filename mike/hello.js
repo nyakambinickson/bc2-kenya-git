ec0ac0025e69c94840c263c613d993d4c68985b6
TheAlmikey <thealmikey@gmail.com>
2015-08-25 15:42:33 +0300

    catz code

--- mike/hello.js
+++ mike/hello.js
@@ -1,8 +1,12 @@
 function catsDonYolo(x){
-   for(var i=1; i<10; i++){
+ if(x < 9){  
+ 	for(var i=1; i<10; i++){
 	console.log("I have "+i+"lives son!");
-   }
+  				 }
+  	}	
    if(x>9){
    console.log("am sorry i cant YOLO for that long");
-   }
+  	 }
 }
+
+catsDonYolo(8);i
